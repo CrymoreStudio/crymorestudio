2c2cae95fa867af1208227c6dbee100a7b1e8538
Viibrant <11376396+Viibrant@users.noreply.github.com>
2023-06-24 02:42:16 +0100

    URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import { SITE_URL } from "./src/data/config";
+// import { SITE_URL } from "./src/data/config";
 
 import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap()],
-  // site: SITE_URL,
+  site: "http://crymore.studio",
   markdown: {
     syntaxHighlight: "shiki",
     shikiConfig: {
