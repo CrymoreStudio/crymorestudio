9b5e4d7f09228524eb76c1517b8e75cd63f61505
Viibrant <11376396+Viibrant@users.noreply.github.com>
2024-08-26 22:24:12 +0100

    feat: remove old projects and add TriggerHoppy

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,31 +3,23 @@ export type Project = {
   techs: string[];
   link: string;
   isComingSoon?: boolean;
+  onHold?: boolean;
 };
 
 const projects: Project[] = [
-  // {
-  //   title: "Dictionary App",
-  //   techs: ["ReactJS (NextJS)", "react-query", "zod"],
-  //   link: "https://github.com/MaeWolff/dictionary-app",
-  // },
-  // {
-  //   title: "Portfolio / Lina BLIDI",
-  //   techs: ["ReactJS (NextJS)", "TypeScript"],
-  //   link: "https://www.linablidi.fr/",
-  // },
   {
-    title: "💊 Trip Shift",
-    techs: ["UE5"],
+    title: "🐸🔫 Trigger Hoppy",
+    techs: ["Unity"],
     link: "/",
     isComingSoon: true,
   },
   {
-    title: "🗡️ Morsfero",
+    title: "💊 Trip Shift",
     techs: ["UE5"],
     link: "/",
-    isComingsoon: true,
+    onHold: true,
   },
+
 ];
 
 export default projects;
